e65140bfa109230eb5571535451752bd712a3cfe
muryoimpl <muryoimpl@gmail.com>
2017-04-24 22:21:48 +0900

    Add entry point of rooms

---
+++ app/javascript/packs/rooms.jsx
@@ -0,0 +1,10 @@
+import 'babel-polyfill';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(
+    <div>HAAAAAAAAAAAAAAAAAI</div>,
+    document.getElementById('talker-signupper'),
+  );
+});
