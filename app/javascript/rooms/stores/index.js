c33b3717a931b000735a2a4f4bf814544cbbe819
muryoimpl <muryoimpl@gmail.com>
2017-05-15 22:38:29 +0900

    Add redux-saga initialization configuration

--- app/javascript/rooms/stores/index.js
+++ app/javascript/rooms/stores/index.js
@@ -1,12 +1,17 @@
 import logger from 'redux-logger';
 import { createStore, applyMiddleware, compose } from 'redux';
+import createSagaMiddleware from 'redux-saga';
 
 import rootReducer from '../reducers';
+import rootSaga from '../sagas';
 
+const sagaMiddleware = createSagaMiddleware({ sagaMonitor: true });
 const finalCreateStore = compose(
-  applyMiddleware(logger),
+  applyMiddleware(logger, sagaMiddleware()),
 )(createStore);
 
 export default function configureStore(initialState) {
-  return finalCreateStore(rootReducer, initialState);
+  const store = finalCreateStore(rootReducer, initialState);
+  sagaMiddleware.run(rootSaga);
+  return store;
 }
