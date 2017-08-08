3d117575006562b1f3820a863d23c8ad3193c957
muryoimpl <muryoimpl@gmail.com>
2017-08-08 23:32:49 +0900

    Fix replacing mistakes

--- app/javascript/rooms/reducers/signups.js
+++ app/javascript/rooms/reducers/signups.js
@@ -7,13 +7,13 @@ export default function signups(signupForm = new SignupForm(), action) {
       return signupForm.merge({ title: action.title });
     case Types.CHANGE_NAME:
       return signupForm.merge({ talker_name: action.talker_name });
-    case Types.CLEAR_SIGNUP_signupForm:
+    case Types.CLEAR_SIGNUP_STATE:
       return signupForm.merge({ title: '', talker_name: '', response: null });
     case Types.STORE_RESPONSE:
       return signupForm.merge({ response: action.response });
     case Types.CLEAR_RESPONSE:
       return signupForm.merge({ response: null });
-    case Types.CHANGE_FORM_signupForm:
+    case Types.CHANGE_FORM_STATE:
       return signupForm.merge({ submitted: action.submitted });
     case Types.REGISTER_SIGNUPPER_TALK:
     default:
