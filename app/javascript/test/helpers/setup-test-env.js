f5aee80598627109b4ab467f789ebab053bd33a1
muryoimpl <muryoimpl@gmail.com>
2017-08-18 00:44:24 +0900

    Remove app/javascript/test directory

--- app/javascript/test/helpers/setup-test-env.js
+++
@@ -1,8 +0,0 @@
-const { JSDOM } = require('jsdom');
-
-const dom = new JSDOM('</body></body>');
-global.document = dom.window.document;
-global.window = dom.window;
-global.navigator = {
-  userAgent: 'node.js',
-};
