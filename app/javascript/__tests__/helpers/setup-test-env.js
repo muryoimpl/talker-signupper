923854595c81a5d543071f3f657228938227dfe8
muryoimpl <muryoimpl@gmail.com>
2017-10-10 01:07:33 +0900

    Add Enzyme configuration to Jest's setup file
    To run test for React 16 apps, it needs to upgrade Enzyme 3. And
    Enzyme v3 needs configuration about adapter(new feature). This app uses
    react 16 so it uses enzyme-adapter-react-16 as adapter.

--- app/javascript/__tests__/helpers/setup-test-env.js
+++ app/javascript/__tests__/helpers/setup-test-env.js
@@ -3,6 +3,11 @@ require('babel-register')
 import 'babel-polyfill'
 import 'raf/polyfill'
 
+import Enzyme from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+
+Enzyme.configure({ adapter: new Adapter() })
+
 const { JSDOM } = require('jsdom');
 
 const dom = new JSDOM('</body></body>');
@@ -11,3 +16,5 @@ global.window = dom.window;
 global.navigator = {
   userAgent: 'node.js',
 };
+
+
