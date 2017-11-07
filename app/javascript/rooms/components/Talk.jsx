f4cc54b5591388545faee02188998c988864dd9c
muryoimpl <muryoimpl@gmail.com>
2017-11-08 00:10:40 +0900

    Add "Talk" design (candidate)

--- app/javascript/rooms/components/Talk.jsx
+++ app/javascript/rooms/components/Talk.jsx
@@ -3,21 +3,41 @@ import PropTypes from 'prop-types';
 import Immutable from 'immutable';
 
 export default class Talk extends React.Component {
+  handleClickStart(e) {
+    e.preventDefault();
+    const talk = this.props.talk.toJS();
+    console.log(talk.id);
+    console.log(talk.room_id);
+  }
+
   render() {
     const { talk } = this.props;
-    const id = talk.get('id');
     const title = talk.get('title');
     const talkerName = talk.get('talker_name');
 
     return (
-      <div key={`${title}-${talkerName}-${id}`}>
-        <span>{title}</span>
-        <span>{talkerName}</span>
-      </div>
+      <section className="p-talk mdl-grid mdl-grid--no-spacing mdl-shadow--3dp">
+        <div className="p-talk-card mdl-card">
+          <div className="p-talk-card__text mdl-card__supporting-text">
+            <h3 className="p-talk-title">{title}</h3>
+            <div className="p-talk-talker">{talkerName}</div>
+          </div>
+        </div>
+
+        { this.props.i === 0 &&
+          <a href="#" className="p-talk-card-button__active mdl-color--indigo-500 mdl-color-text--white" onClick={e => this.handleClickStart(e)}>
+            <i className="material-icons">play_circle_filled</i>
+          </a>
+        }
+        { this.props.i !== 0 &&
+          <header className="p-talk-card-button mdl-color--teal-100 mdl-color-text--white" />
+        }
+      </section>
     );
   }
 }
 
 Talk.propTypes = {
   talk: PropTypes.instanceOf(Immutable.Map).isRequired,
+  i: PropTypes.number.isRequired,
 };
