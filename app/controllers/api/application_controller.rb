58f8b2e25086a7ab4fcf1180f81c0638adecc67e
muryoimpl <muryoimpl@gmail.com>
2017-11-14 00:30:34 +0900

    Log error when 404 error occurs

--- app/controllers/api/application_controller.rb
+++ app/controllers/api/application_controller.rb
@@ -1,13 +1,24 @@
 # frozen_string_literal: true
 class Api::ApplicationController < ActionController::API
-  def render_404
+  def render_404(e = nil)
+    log_error(e)
     head :not_found
   end
 
+  private
+
   def log_error(e)
+    return unless e
     Rails.logger.error(<<~ERR)
       #{e.class} #{e.message}
       #{e.backtrace.join("\n")}
     ERR
+  end
+
+  def log_warn(e)
+    Rails.logger.warn(<<~WARN)
+      #{e.class} #{e.message}
+      #{e.backtrace.join("\n")}
+    WARN
   end
 end
