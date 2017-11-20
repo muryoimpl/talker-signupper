e92466a54f3d80bf525d200acc23b5dbd35281f6
muryoimpl <muryoimpl@gmail.com>
2017-11-21 00:57:09 +0900

    Add tests for components

---
+++ app/javascript/__tests__/rooms/components/Talk.test.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Immutable from 'immutable';
+
+import Talk from '../../../rooms/components/Talk';
+
+test('show talk information', () => {
+  const talkJSON = { id: 1, title: 'hi', talker_name: 'muryoimpl', room_id: 1 };
+  const talk = new Immutable.Map(talkJSON);
+  const wrapper = shallow(<Talk i={0} talk={talk} />);
+
+  expect(
+    wrapper.containsMatchingElement(
+      <div className="p-talk-card mdl-card">
+        <div className="p-talk-card__text mdl-card__supporting-text">
+          <h3 className="p-talk-title">hi</h3>
+          <div className="p-talk-talker">muryoimpl</div>
+        </div>
+      </div>,
+    ),
+  ).toBe(true);
+});
+
+test('show link to start talk if it is first talk', () => {
+  const talkJSON = { id: 1, title: 'hi', talker_name: 'muryoimpl', room_id: 1 };
+  const talk = new Immutable.Map(talkJSON);
+  const wrapper = shallow(<Talk i={0} talk={talk} />);
+
+  expect(
+    wrapper.containsMatchingElement(
+      <a href="#" className="p-talk-card-button__active mdl-color--indigo-500 mdl-color-text--white">
+        <i className="material-icons">play_circle_filled</i>
+      </a>,
+    ),
+  ).toBe(true);
+});
+
+test('show element which is not a link if it is not first talk', () => {
+  const talkJSON = { id: 1, title: 'hi', talker_name: 'muryoimpl', room_id: 1 };
+  const talk = new Immutable.Map(talkJSON);
+  const wrapper = shallow(<Talk i={1} talk={talk} />);
+
+  expect(
+    wrapper.containsMatchingElement(
+      <header className="p-talk-card-button mdl-color--teal-100 mdl-color-text--white" />,
+    ),
+  ).toBe(true);
+});
---
+++ app/javascript/__tests__/rooms/components/Talks.test.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import configureStore from 'redux-mock-store';
+import { Provider } from 'react-redux';
+import Immutable from 'immutable';
+import { TransitionGroup } from 'react-transition-group';
+
+import Talks from '../../../rooms/components/Talks';
+import NoEntry from '../../../rooms/components/NoEntry';
+import TalksGroup from '../../../rooms/components/TalksGroup';
+import Talk from '../../../rooms/components/Talk';
+
+const mockStore = configureStore();
+// prevent App.talks and App.cable.subscriptions.create from operating anything
+global.App = { talks: null, cable: { subscriptions: { create: (_a, _b) => [_a, _b] } } };
+
+test('Show loading', () => {
+  const initialState = { talks: { loading: true, entries: new Immutable.List() } };
+  const store = mockStore(initialState);
+  const wrapper = mount(<Provider store={store}><Talks name={'aaaa'} /></Provider>);
+
+  expect(
+    wrapper.contains(<div className="mdl-spinner mdl-spinner--single-color mdl-js-spinner is-active" />),
+  ).toBe(true);
+});
+
+test('Show noEntry component', () => {
+  const initialState = { talks: { loading: false, entries: new Immutable.List() } };
+  const store = mockStore(initialState);
+  const wrapper = mount(<Provider store={store}><Talks name={'aaaa'} /></Provider>);
+
+  expect(wrapper.contains(<NoEntry />)).toBe(true);
+});
+
+test('Show talk', () => {
+  const entry = new Immutable.Map({ id: 1, talker_name: 'muryoimpl', title: 'hi' });
+  const initialState = { talks: { loading: false, entries: new Immutable.List([entry]) } };
+  const store = mockStore(initialState);
+  const wrapper = mount(<Provider store={store}><Talks name={'aaaa'} /></Provider>);
+
+  expect(
+    wrapper.contains(
+      <div>
+        <TransitionGroup>
+          {initialState.talks.entries.map((talk, i) => (
+            <TalksGroup timeout={300} key={`talk-group-${talk.get('id')}`}>
+              <Talk talk={talk} key={talk.get('id')} i={i} />
+            </TalksGroup>
+          ))}
+        </TransitionGroup>
+      </div>,
+
+    ),
+  ).toBe(true);
+});
