fca1f908059a76987ddb0c29157609fbcd67b551
muryoimpl <muryoimpl@gmail.com>
2017-11-21 00:54:00 +0900

    Remove unused jsdom configuration

--- app/javascript/__tests__/helpers/setup-test-env.js
+++ app/javascript/__tests__/helpers/setup-test-env.js
@@ -8,12 +8,3 @@ import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 Enzyme.configure({ adapter: new Adapter() });
-
-const { JSDOM } = require('jsdom');
-
-const dom = new JSDOM('</body></body>');
-global.document = dom.window.document;
-global.window = dom.window;
-global.navigator = {
-  userAgent: 'node.js',
-};
