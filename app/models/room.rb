5aef2802df6db311439d86335d52281cf2e062fa
muryoimpl <muryoimpl@gmail.com>
2018-01-02 02:59:58 +0900

    Add `dependent: :destroy` to talks relation

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 class Room < ApplicationRecord
   ALLOWED_PATTERN = /\A[0-9a-zA-Z!$\+\-@_]+\z/
-  has_many :talks
+  has_many :talks, dependent: :destroy
 
   validates :name, presence: true, length: {in: 4..30, allow_blank: true}, uniqueness: true
   validates :name, format: {with: ALLOWED_PATTERN, message: I18n.t('errors.not_allowed_character'), allow_blank: true}
