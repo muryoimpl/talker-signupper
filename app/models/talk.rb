53379019a20ab3ee9c0ec6ec70f7a1fe24720793
muryoimpl <muryoimpl@gmail.com>
2018-03-10 13:35:54 +0900

    Limit title and talker name length

--- app/models/talk.rb
+++ app/models/talk.rb
@@ -2,8 +2,9 @@
 class Talk < ApplicationRecord
   belongs_to :room
 
-  validates :title, presence: true, uniqueness: {scope: :room_id, message: I18n.t('errors.messages.taken')}
-  validates :talker_name, presence: true
+  validates :title, presence: true, length: {maximum: 50}
+  validates :title, uniqueness: {scope: :room_id, message: I18n.t('errors.messages.taken')}
+  validates :talker_name, presence: true, length: {maximum: 50}
 
   scope :ordered_by_number, -> { order(order_number: :desc, id: :asc) }
 
