6085030d85f4686d8ae14fcb8ba336400ddd166d
muryoimpl <muryoimpl@gmail.com>
2018-03-15 15:22:05 +0900

    :cop: Fix Style/PercentLiteralDelimiters

--- config/initializers/exception_app.rb
+++ config/initializers/exception_app.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 Rails.configuration.exceptions_app = ->(env) {
-  if %r{\A/api}.match?(env['REQUEST_URI'])
+  if %r|\A/api|.match?(env['REQUEST_URI'])
     Api::ApplicationController.action(:render_404).call(env)
   else
     ApplicationController.action(:render_404).call(env)
