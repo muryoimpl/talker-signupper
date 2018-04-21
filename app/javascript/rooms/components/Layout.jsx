8793dc716e03298e59e0e08ac082b2ff6d369870
muryoimpl <muryoimpl@gmail.com>
2018-04-21 23:32:39 +0900

    Refactor Layout component

--- app/javascript/rooms/components/Layout.jsx
+++ app/javascript/rooms/components/Layout.jsx
@@ -5,18 +5,14 @@ import MessageDialog from './MessageDialog';
 import Header from './Header';
 import TimerDialog from './TimerDialog';
 
-class Layout extends React.Component {
-  render() {
-    return (
-      <div className="content">
-        <MessageDialog />
-        <TimerDialog />
-        <Header />
-        {this.props.children}
-      </div>
-    );
-  }
-}
+const Layout = ({ children }) => (
+  <div className="content">
+    <MessageDialog />
+    <TimerDialog />
+    <Header />
+    {children}
+  </div>
+);
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
