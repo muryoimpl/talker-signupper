c352c6c574fb65c2d8e83494e2e0f7996723d762
muryoimpl <muryoimpl@gmail.com>
2018-05-30 01:43:28 +0900

    Add tests for MessageDialog

--- app/javascript/rooms/components/tests/MessageDialog.test.js
+++ app/javascript/rooms/components/tests/MessageDialog.test.js
@@ -4,6 +4,7 @@ import configureStore from 'redux-mock-store';
 import { Provider } from 'react-redux';
 
 import MessageDialog from '../MessageDialog';
+import DialogContent from '../presentationals/MessageDialogContent';
 
 const mockStore = configureStore();
 
@@ -21,4 +22,48 @@ test('close dialog when clicking close button', () => {
   wrapper.find('button.close').simulate('click');
   const actions = store.getActions();
   expect(actions).toEqual([{ type: 'CLOSE_DIALOG' }]);
+});
+
+test('show dialog and click close button', () => {
+  const closeAction = jest.fn();
+  const showAction = jest.fn();
+  const wrapper = mount(
+    <DialogContent
+      isDisplay={false}
+      message="hi"
+      handleClickClose={closeAction}
+      handleShowDialog={showAction}
+    />,
+  );
+
+  expect(closeAction.mock.calls.length).toEqual(0);
+  expect(showAction.mock.calls.length).toEqual(0);
+
+  wrapper.setProps({ message: 'hihihi', isDisplay: true });
+  expect(closeAction.mock.calls.length).toEqual(0);
+  expect(showAction.mock.calls.length).toEqual(1);
+
+  wrapper.find('button.close').simulate('click');
+  expect(closeAction.mock.calls.length).toEqual(1);
+  expect(showAction.mock.calls.length).toEqual(1);
+});
+
+test('does not show dialog if only message is updated', () => {
+  const closeAction = jest.fn();
+  const showAction = jest.fn();
+  const wrapper = mount(
+    <DialogContent
+      isDisplay={false}
+      message="hi"
+      handleClickClose={closeAction}
+      handleShowDialog={showAction}
+    />,
+  );
+
+  expect(closeAction.mock.calls.length).toEqual(0);
+  expect(showAction.mock.calls.length).toEqual(0);
+
+  wrapper.setProps({ message: 'hihihi' });
+  expect(closeAction.mock.calls.length).toEqual(0);
+  expect(showAction.mock.calls.length).toEqual(0);
 });
