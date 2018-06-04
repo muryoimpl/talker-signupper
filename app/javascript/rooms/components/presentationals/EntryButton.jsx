7e0fe66bffa514d51eece9116d82bb4d23bc9f18
muryoimpl <muryoimpl@gmail.com>
2018-06-04 23:02:05 +0900

    Remove unfunctional function

--- app/javascript/rooms/components/presentationals/EntryButton.jsx
+++ app/javascript/rooms/components/presentationals/EntryButton.jsx
@@ -1,16 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const clearDirty = () => {
-  const mdlInputs = document.querySelectorAll('.mdl-js-textfield');
-  for (let i = 0, l = mdlInputs.length; i < l; i += 1) {
-    mdlInputs[i].MaterialTextfield.checkDirty();
-  }
-};
-
 const handleClickSignUp = (e, showDialogFunc) => {
   e.preventDefault();
-  clearDirty();
   document.querySelector('dialog#signup-form').showModal();
   showDialogFunc();
 };
